6474ed4618b8254c2e6a714f94755d8ed73feac5
Afeez G. Lawal <afeezlg@gmail.com>
2021-04-28 09:35:43 -0700

    added str method to todo model

--- api/models.py
+++ api/models.py
@@ -4,3 +4,6 @@ class Todo(models.Model):
     text = models.CharField(max_length=150)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
