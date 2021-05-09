d9dfbc43bb70e6c8133881a914de1c8cd6a82771
Afeez G. Lawal <afeezlg@gmail.com>
2021-05-09 02:15:18 -0700

    added uuid feature

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -10,6 +10,23 @@ const AddTask = () => {
         setText(e.target.value)
     }
 
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    const csrftoken = getCookie('csrftoken');
+
     const submitForm = (e) => {
         e.preventDefault()
         let deviceId = window.localStorage.getItem("uuid")
@@ -17,7 +34,9 @@ const AddTask = () => {
         formData.append("text", text)
         formData.append("uuid", deviceId)
         fetch("/api/todo/", {
+            headers: {'X-CSRFToken': csrftoken},
             method: "POST",
+            mode: "same-origin",
             body: formData
         })
         .then(res => res.json())
